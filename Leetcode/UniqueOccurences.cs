b0909ba39d06b3dd35d1656b38593f21c8057205
CTR281 <chmartin2.81@gmail.com>
2024-01-17 19:15:05 +0100

    add UniqueOccurences (daily)

---
+++ Leetcode/UniqueOccurences.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class UniqueOccurences // https://leetcode.com/problems/unique-number-of-occurrences/solutions/4583214/map-set-return-early/ EASY
+    {
+        public class Solution
+        {
+            public bool UniqueOccurrences(int[] arr)
+            {
+                Dictionary<int, int> valCount = new();
+                foreach (int val in arr) if (!valCount.TryAdd(val, 1)) valCount[val]++;
+
+                HashSet<int> occurences = new();
+                foreach ((int val, int occurence) in valCount) if (!occurences.Add(occurence)) return false;
+                return true;
+            }
+        }
+    }
+}
