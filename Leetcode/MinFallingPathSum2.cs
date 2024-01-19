d5d97afddadc332e2f2d67e642bb3b7aaba2dc8c
CTR281 <chmartin2.81@gmail.com>
2024-01-19 22:25:50 +0100

    add MinFallingPathSum2

---
+++ Leetcode/MinFallingPathSum2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class MinFallingPathSum2
+    {
+        public class Solution
+        {
+            public int MinFallingPathSum(int[][] grid)
+            {
+                int n = grid.Length;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    PriorityQueue<int, int> heap = new PriorityQueue<int, int>(); // Min-heap
+                    for (int j =  0; j < n; j++)
+                    {
+                        heap.Enqueue(grid[n - 1 - i][j], grid[n - 1 - i][j]);
+                    }
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (grid[n - 1 - i][j] == heap.Peek()) heap.Dequeue();
+                        grid[n - 2 - i][j] += heap.Peek();
+                        heap.Enqueue(grid[n - 1 - i][j], grid[n - 1 - i][j]);                       
+                    }
+                }
+                return grid[0].Min();
+            }
+        }
+    }
+}
