abec355e983eddbcac904707ef2b901a55d0c89c
CTR281 <chmartin2.81@gmail.com>
2024-01-20 23:44:20 +0100

    add LargestRectangleArea

---
+++ Leetcode/LargestRectangleArea.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class LargestRectangleArea // https://leetcode.com/problems/largest-rectangle-in-histogram/
+    {
+        public class Solution
+        {
+            public int LargestRectangleArea(int[] heights)
+            {
+                List<int> A = heights.ToList();
+                A.Add(0);
+                Stack<int> stack = new();
+                int res = 0;
+                for (int k = 0; k < A.Count; k++)
+                {
+                    while (stack.Count > 0 && A[stack.Peek()] > A[k])
+                    {
+                        int height = A[stack.Pop()];
+                        int left = stack.Count > 0 ? stack.Peek() : -1;
+                        res = Math.Max(res, (k - left - 1) * height);
+                    }
+                    stack.Push(k);
+                }
+                return res;
+            }
+        }
+    }
+}
