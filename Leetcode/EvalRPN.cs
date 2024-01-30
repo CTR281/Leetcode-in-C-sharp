4a3506b33a77f48cd6381f57577c52c685ab063a
CTR281 <chmartin2.81@gmail.com>
2024-01-30 18:07:34 +0100

    add EvalRPN (daily)

---
+++ Leetcode/EvalRPN.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    internal class EvalRPN // https://leetcode.com/problems/evaluate-reverse-polish-notation/
+    {
+        public class Solution
+        {
+            public int EvalRPN(string[] tokens)
+            {
+                Stack<int> stack = new Stack<int>();
+
+                for (int k = 0; k < tokens.Length; k++)
+                {
+                    if (Int32.TryParse(tokens[k], out int x)) stack.Push(x);
+                    else
+                    {
+                        int x1 = stack.Pop();
+                        int x2 = stack.Pop();
+
+                        switch (tokens[k])
+                        {
+                            case "+": stack.Push(x2 + x1); break;
+                            case "-": stack.Push(x2 - x1); break;
+                            case "*": stack.Push(x2 * x1); break;
+                            case "/": stack.Push(x2 / x1); break;
+                        }
+                    }
+                }
+                return stack.Pop();
+            }
+        }
+    }
+}
