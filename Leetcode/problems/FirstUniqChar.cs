79199ca93e8f6e56b6107f73db8ebcede66e567b
CTR281 <chmartin2.81@gmail.com>
2024-02-05 21:28:56 +0100

    add FirstUniqChar (daily)

---
+++ Leetcode/problems/FirstUniqChar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems
+{
+    internal class FirstUniqChar // https://leetcode.com/problems/first-unique-character-in-a-string/
+    {
+        public class Solution
+        {
+            public int FirstUniqChar(string s)
+            {
+                int[] chars = new int[26];
+                int n = s.Length;
+
+                foreach (char c in s)
+                {
+                    chars[c - 'a']++;                   
+                }
+                for (int k = 0; k < n; k++)
+                {
+                    if (chars[s[k] - 'a'] == 1) return k;
+                }
+                return -1;
+            }
+        }
+    }
+}
