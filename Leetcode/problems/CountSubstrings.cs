04431c3f403cff30ecb7af935de23a2a96af9c0e
CTR281 <chmartin2.81@gmail.com>
2024-02-10 16:58:57 +0100

    :fire: remove solution and add brute force solution (CountSubstrings - daily)

--- Leetcode/problems/CountSubstrings.cs
+++ Leetcode/problems/CountSubstrings.cs
@@ -4,48 +4,6 @@
     {
         public class Solution
         {
-            private Dictionary<(int, int), bool> memo = new();
-
-            public int CountSubstrings(string s)
-            {
-                int result = 0;
-                for (int i = 0; i < s.Length; i++)
-                {
-                    for (int j = i; j < s.Length; j++)
-                    {
-                        result += dp(i, j, s) ? 1 : 0;
-                    }
-                }
-                // Console.WriteLine(string.Join(Environment.NewLine, memo.Select(kvp => kvp.Key + ": " + kvp.Value.ToString())));
-                return result;
-            }
-
-            private bool dp(int i, int j, string s)
-            {
-                if (memo.ContainsKey((i, j))) return memo[(i, j)];
-                if (j - i == 1)
-                {
-                    memo.Add((i, j), s[i] == s[j]);
-                    return memo[(i, j)];
-                }
-                if (j == i)
-                {
-                    memo.Add((i, j), true);
-                    return memo[(i, j)];
-                }
-                bool result = true;
-                int m = (j - i) / 2;
-                for (int k = 0; k <= m; k++)
-                {
-                    result &= s[i + k] == s[j - k];
-                }
-                memo.Add((i, j), result);
-                return result;
-            }
-        }
-
-        public class Solution2
-        {
             public int CountSubstrings(string s)
             {
                 int result = 0;
@@ -70,7 +28,7 @@
             }
         }
 
-        public class Solution3
+        public class Solution2 // DP
         {
             public int CountSubstrings(string s)
             {
@@ -90,6 +48,29 @@
                     }
                 }
                 return result;
+            }
+        }
+
+        public class Solution3 // O(N^3)
+        {
+            public int CountSubstrings(string s)
+            {
+                int n = s.Length;
+
+                int res = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = i; j < n; j++)
+                    {
+                        bool isPalindrom = true;
+                        for (int k = 0; k <= (j - i) / 2; k++)
+                        {
+                            if (s[i + k] != s[j - k]) isPalindrom = false;
+                        }
+                        if (isPalindrom) res++;
+                    }
+                }
+                return res;
             }
         }
     }
