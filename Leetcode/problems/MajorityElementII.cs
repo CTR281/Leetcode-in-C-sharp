d057228b79cd6ded3a7698675dadec7be60f60e0
CTR281 <chmartin2.81@gmail.com>
2024-02-12 21:45:04 +0100

    :sparkles: add MajorityElementII

---
+++ Leetcode/problems/MajorityElementII.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems
+{
+    internal class MajorityElementII // https://leetcode.com/problems/majority-element-ii/
+    {
+        public class Solution
+        {
+            public IList<int> MajorityElementII(int[] nums)
+            {
+                int element1 = 0, element2 = 0, count1 = 0, count2 = 0;
+                foreach (int num in nums)
+                {
+                    if (count1 == 0 && num != element2)
+                    {
+                        count1 = 1;
+                        element1 = num;
+                    }
+                    else if (count2 == 0 && num != element1)
+                    {
+                        count2 = 1;
+                        element2 = num;
+                    }
+                    else if (num == element1) count1++;
+                    else if (num == element2) count2++;
+                    else
+                    {
+                        count1--;
+                        count2--;
+                    }
+                }
+
+                List<int> result = new List<int>();
+
+                count1 = 0; count2 = 0;
+                foreach(int num in nums)
+                {
+                    if (num == element1) count1++;
+                    if (num == element2) count2++;
+                }
+                if (count1 > nums.Length / 3) result.Add(element1);
+                if (count2 > nums.Length / 3 && element2 != element1) result.Add(element2);
+
+                return result;
+            }
+        }
+    }
+}
