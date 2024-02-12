4f4573f2dd6681440d6768542ac273602d564008
CTR281 <chmartin2.81@gmail.com>
2024-02-12 20:47:50 +0100

    :sparkles: add MajorityElement (daily)

---
+++ Leetcode/problems/MajorityElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems
+{
+    internal class MajorityElement // https://leetcode.com/problems/majority-element/
+    {
+        public class Solution
+        {
+            public int MajorityElement(int[] nums)
+            {
+                int count = 0; int result = 0;
+                foreach (int num in nums)
+                {
+                    if (count == 0) result = num;
+                    if (result != num) count--; else count++;
+                }
+                return result;
+            }
+        }
+    }
+}
