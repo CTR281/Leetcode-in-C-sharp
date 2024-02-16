916498d1d63c0d0a18f7fb871f022d32cd78b551
CTR281 <chmartin2.81@gmail.com>
2024-02-16 09:25:36 +0100

    :sparkles: add LargestPerimeter (daily)

---
+++ Leetcode/problems/LargestPerimeter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems
+{
+    internal class LargestPerimeter // https://leetcode.com/problems/find-polygon-with-the-largest-perimeter/
+    {
+        public class Solution
+        {
+            public long LargestPerimeter(int[] nums)
+            {
+                Array.Sort(nums);
+                int n = nums.Length;
+                long result = -1;
+                int candidate = 0;
+
+                for (int k = 0; k < n; k++)
+                {
+                    if (k > 1 && candidate > nums[k]) result = candidate + nums[k];
+                    candidate += nums[k];
+                }
+
+                return result;
+            }
+        }
+    }
+}
