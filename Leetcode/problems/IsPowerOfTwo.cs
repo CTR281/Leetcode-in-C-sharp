637305fcf25d08f8eb9666e0f2e65931a255d0aa
CTR281 <chmartin2.81@gmail.com>
2024-02-19 19:45:06 +0100

    :sparkles: add isPowerTwo (daily)

---
+++ Leetcode/problems/IsPowerOfTwo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems
+{
+    internal class IsPowerOfTwo // https://leetcode.com/problems/power-of-two/
+    {
+        public class Solution
+        {
+            public bool IsPowerOfTwo(int n)
+            {
+                return n > 0 && int.PopCount(n) == 1;
+            }
+        }
+
+        public class Solution2
+        {
+            public bool IsPowerOfTwo(int n)
+            {
+                return n > 0 && (n & (n - 1)) == 0;
+            }
+        }
+    }
+}
