e6b2412901a97e44956cd3e72d1ccb044c285b21
CTR281 <chmartin2.81@gmail.com>
2024-02-21 13:14:43 +0100

    :sparkles: add RangeBitwiseAnd (daily)

---
+++ Leetcode/problems/RangeBitwiseAnd.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems
+{
+    internal class RangeBitwiseAnd // https://leetcode.com/problems/bitwise-and-of-numbers-range/
+    {
+        public class Solution
+        {
+            private int SignificantBit(int num)
+            {
+                if (num / 2 > 0) return 1 + SignificantBit(num / 2);
+                return 0;
+            }
+
+            public int RangeBitwiseAnd(int left, int right)
+            {
+                int result = 0;
+                int shift = SignificantBit(left);
+                while (shift == SignificantBit(right) && left != 0)
+                {
+                    result += 1 << shift;
+                    left &= (1 << shift) - 1;
+                    right &= (1 << shift) - 1;
+                    shift = SignificantBit(left);
+                }
+                return result;
+            }
+        }
+
+        public class Solution2
+        {
+            public int RangeBitwiseAnd(int left, int right) // Find common prefix
+            {
+                int cnt = 0;
+                while (left != right)
+                {
+                    left >>= 1;
+                    right >>= 1;
+                    cnt++;
+                }
+                return (left << cnt);
+            }
+        }
+    }
+}
