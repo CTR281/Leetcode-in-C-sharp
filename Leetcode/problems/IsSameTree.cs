49b4b199ba1d30a8c3ec2d6bc0986d2612d75db3
CTR281 <chmartin2.81@gmail.com>
2024-02-26 21:03:15 +0100

    :sparkles: add IsSameTree (daily)

---
+++ Leetcode/problems/IsSameTree.cs
@@ -0,0 +1,21 @@
+﻿using Leetcode.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems
+{
+    internal class IsSameTree // https://leetcode.com/problems/same-tree/
+    {
+        public class Solution
+        {
+            public bool IsSameTree(TreeNode p, TreeNode q)
+            {
+                if (p == null || q == null) return p == q;
+                return p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+            }
+        }
+    }
+}
