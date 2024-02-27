946eb8869b6385b316d981ae8a861399f33736f2
CTR281 <chmartin2.81@gmail.com>
2024-02-27 20:24:44 +0100

    :sparkles: add DiameterOfBinaryTree (daily)

---
+++ Leetcode/problems/DiameterOfBinaryTree.cs
@@ -0,0 +1,30 @@
+﻿using Leetcode.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems
+{
+    internal class DiameterOfBinaryTree // https://leetcode.com/problems/diameter-of-binary-tree/
+    {
+        public class Solution
+        {
+            private int diameter = 0;
+            private int dfs(TreeNode node)
+            {
+                if (node == null) return -1;
+                int left = 1 + dfs(node.left);
+                int right = 1 + dfs(node.right);
+                diameter = Math.Max(diameter, left + right);
+                return Math.Max(left, right);
+            }
+            public int DiameterOfBinaryTree(TreeNode root)
+            {
+                dfs(root);
+                return diameter;
+            }
+        }
+    }
+}
