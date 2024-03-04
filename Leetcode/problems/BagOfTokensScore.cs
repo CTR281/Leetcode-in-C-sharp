80bd14ff4d48c554b0ee32820ab135d7cd8f967d
CTR281 <chmartin2.81@gmail.com>
2024-03-04 09:22:53 +0100

    :sparkles: add BagOfTokensScore (daily)

---
+++ Leetcode/problems/BagOfTokensScore.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems
+{
+    internal class BagOfTokensScore // https://leetcode.com/problems/bag-of-tokens/
+    {
+        public class Solution
+        {
+            public int BagOfTokensScore(int[] tokens, int power)
+            {
+                int result = 0;
+                int score = 0;
+                int n = tokens.Length;
+                Array.Sort(tokens);
+
+                int right = n - 1;
+                int left = 0;
+                while (left <= right)
+                {
+                    if (power < tokens[left])
+                    {
+                        if (score > 0)
+                        {
+                            power += tokens[right--];
+                            score--;
+                        }
+                        else return result;
+                    }
+                    else
+                    {
+                        power -= tokens[left++];
+                        score++;
+                    }
+                    result = Math.Max(result, score);
+                }
+
+                return result;
+            }
+        }
+    }
+}
