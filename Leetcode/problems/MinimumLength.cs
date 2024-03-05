4e0548378586baea37c715c36baa7294d5d46e2e
CTR281 <chmartin2.81@gmail.com>
2024-03-05 13:56:57 +0100

    :sparkles: add MinimumLength (daily)

---
+++ Leetcode/problems/MinimumLength.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems
+{
+    internal class MinimumLength // https://leetcode.com/problems/minimum-length-of-string-after-deleting-similar-ends/
+    {
+        public class Solution
+        {
+            public int MinimumLength(string s)
+            {
+                int left = 0;
+                int right = s.Length - 1;
+
+                while (left < right && s[left] == s[right])
+                {
+                    char c = s[left];
+                    while (left <= right && s[left] == c) left++;
+                    while (right > left && s[right] == c) right--;
+                }
+
+                return right - left + 1;
+            }
+        }
+
+        public class Solution2
+        {
+            public int MinimumLength(string s)
+            {
+                int left = 0;
+                int right = s.Length - 1;
+
+                while (left < right && s[left] == s[right])
+                {
+                    char c = s[left];
+                    while (left < s.Length && s[left] == c) left++;
+                    while (right >= 0 && s[right] == c) right--;
+
+                }
+
+                return Math.Max(right - left + 1, 0);
+            }
+        }
+    }
+}
