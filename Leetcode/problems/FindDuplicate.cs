818ef39af5f6459b807c62a37bb9e19c90acaa5d
CTR281 <chmartin2.81@gmail.com>
2024-03-06 09:19:47 +0100

    :sparkles: add FindDuplicate

---
+++ Leetcode/problems/FindDuplicate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems
+{
+    internal class FindDuplicate
+    {
+        public class Solution
+        {
+            public int FindDuplicate(int[] nums)
+            {
+                int n = nums.Length;
+                int result = 0;
+                foreach (int num in nums)
+                {
+                    result += num;
+                }
+                return (n - 1) * (n) / 2 - result;
+            }
+        }
+    }
+}
