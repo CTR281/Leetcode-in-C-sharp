9374afa19f578e8998a866c300ba924dd9085bf2
CTR281 <chmartin2.81@gmail.com>
2024-03-06 09:14:36 +0100

    :sparkles: add HasCycle (daily)

---
+++ Leetcode/problems/HasCycle.cs
@@ -0,0 +1,31 @@
+﻿using Leetcode.models.leetcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems
+{
+    internal class HasCycle // https://leetcode.com/problems/linked-list-cycle/
+    {
+        public class Solution
+        {
+            public bool HasCycle(ListNode head)
+            {
+                if (head == null) return false;
+                ListNode slow = head;
+                ListNode fast = head.next;
+                int k = 0;
+
+                while (fast != slow) {
+                    if (k++ % 2 == 0) slow = slow.next;
+                    if (fast == null) return false;
+                    fast = fast.next;
+                }
+
+                return true;
+            }
+        }
+    }
+}
