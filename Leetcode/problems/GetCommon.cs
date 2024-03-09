d817ab2110361697fbf8b34cc4f4584dc98b61b2
CTR281 <chmartin2.81@gmail.com>
2024-03-09 09:42:06 +0100

    :sparkles: add GetCommon (daily)

---
+++ Leetcode/problems/GetCommon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems
+{
+    internal class GetCommon // https://leetcode.com/problems/minimum-common-value/
+    {
+        public class Solution
+        {
+            public int GetCommon(int[] nums1, int[] nums2)
+            {
+                int cur1 = 0, cur2 = 0;
+
+                while (nums1[cur1] != nums2[cur2])
+                {
+                    while (nums1[cur1] < nums2[cur2])
+                    {
+                        if (++cur1 == nums1.Length) return -1;
+                    }
+                    while (nums2[cur2] < nums1[cur1])
+                    {
+                        if (++cur2 == nums2.Length) return -1;
+                    }
+                }
+
+                return nums1[cur1];
+            }
+        }
+    }
+}
