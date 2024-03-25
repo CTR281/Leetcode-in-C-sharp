db865b858f595b6bf85d11dddd3bb2d98eb7369a
CTR281 <chmartin2.81@gmail.com>
2024-03-25 09:20:36 +0100

    :sparkles: add FindMinArrowShots (daily)

---
+++ Leetcode/problems/2-medium/FindMinArrowShots.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class FindMinArrowShots // https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+    {
+        class Solution
+        {
+            public int FindMinArrowShots(int[][] points)
+            {
+                Array.Sort(points, (a, b) => b[1] - a[1]);
+                int shotCount = 1;
+                int cursor = 1;
+                int bound = points[0][0];
+                while (cursor < points.Length)
+                {
+                    if (bound <= points[cursor][1])
+                    {
+                        bound = Math.Max(bound, points[cursor][0]);
+                    }
+                    else
+                    {
+                        shotCount++;
+                        bound = points[cursor][0];
+                    }
+                    cursor++;
+                }
+                return shotCount;
+            }
+        }
+    }
+}
