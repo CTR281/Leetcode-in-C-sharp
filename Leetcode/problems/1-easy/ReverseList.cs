59097cd0a3604d7947bd4d05a38697a657adba1a
CTR281 <chmartin2.81@gmail.com>
2024-03-25 09:23:15 +0100

    :sparkles: add ReverseList (daily)

---
+++ Leetcode/problems/1-easy/ReverseList.cs
@@ -0,0 +1,27 @@
+﻿using Leetcode.models.leetcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._1_easy
+{
+    internal class ReverseList // https://leetcode.com/problems/reverse-linked-list/
+    {
+        public class Solution
+        {
+            private ListNode helper(ListNode node, ListNode parent)
+            {
+                if (node == null) return parent;
+                ListNode head = helper(node.next, node);
+                node.next = parent;
+                return head;
+            }
+            public ListNode ReverseList(ListNode head)
+            {
+                return helper(head, null);
+            }
+        }
+    }
+}
