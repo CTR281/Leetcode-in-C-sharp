15598ce3e8c32a47c79b123f7464c37ff232fe5a
CTR281 <chmartin2.81@gmail.com>
2024-03-27 20:13:21 +0100

    :sparkles: add NumSubarrayProductLessThanK (daily)

---
+++ Leetcode/problems/2-medium/NumSubarrayProductLessThanK.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class NumSubarrayProductLessThanK // https://leetcode.com/problems/subarray-product-less-than-k/
+    {
+        public class Solution
+        {
+            public int NumSubarrayProductLessThanK(int[] nums, int k)
+            {
+                int n = nums.Length, left = 0, right = 0, product = nums[left], result = 0;
+
+                while (left < n && right < n)
+                {
+                    while (product < k && right < n)
+                    {
+                        result += right++ - left + 1;
+                        if (right != n) product *= nums[right];
+                    }
+                    while (product >= k && left <= right && right < n)
+                    {
+                        product /= nums[left++];
+                    }
+                    if (left > right && left < n)
+                    {
+                        right = left;
+                        product = nums[left];
+                    }
+                }
+
+                return result;
+            }
+        }
+
+        public class Solution2
+        {
+            public int NumSubarrayProductLessThanK(int[] nums, int k)
+            {
+                int n = nums.Length, left = 0, right = 0, product = 1, result = 0;
+                if (k <= 1) return 0;
+                while (right < n)
+                {
+                    product *= nums[right];
+                    while (product >= k) product /= nums[left++];
+                    result += right - left + 1;
+                    right++;
+                }
+
+                return result;
+            }
+        }
+    }
+}
