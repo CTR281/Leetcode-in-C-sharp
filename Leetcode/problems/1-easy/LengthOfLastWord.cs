9321d0c869496fc75f401d55afdea30ebd3552de
CTR281 <chmartin2.81@gmail.com>
2024-04-01 09:29:09 +0200

    :sparkles: add LengthOfLastWord (daily)

---
+++ Leetcode/problems/1-easy/LengthOfLastWord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._1_easy
+{
+    internal class LengthOfLastWord // https://leetcode.com/problems/length-of-last-word/
+    {
+        public class Solution
+        {
+            public int LengthOfLastWord(string s)
+            {
+                string[] splitted = s.Trim().Split(' ');
+                return splitted[splitted.Length - 1].Length;
+            }
+        }
+    }
+}
