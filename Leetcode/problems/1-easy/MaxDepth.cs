fd47b1f1108ebb282d1fcfe925de4b731bedd991
CTR281 <chmartin2.81@gmail.com>
2024-04-04 18:31:31 +0200

    :sparkles: add MaxDepth (daily)

---
+++ Leetcode/problems/1-easy/MaxDepth.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._1_easy
+{
+    internal class MaxDepth // https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+    {
+        public class Solution
+        {
+            public int MaxDepth(string s)
+            {
+                int count = 0, result = 0;
+
+                foreach (char c in s)
+                {
+                    if (c == '(') count++;
+                    else if (c == ')') count--;
+                    result = Math.Max(result, count);
+                }
+                return result;
+            }
+        }
+    }
+}
