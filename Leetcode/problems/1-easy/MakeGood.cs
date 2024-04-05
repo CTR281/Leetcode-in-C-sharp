d01f9461620f05e41fb9fe9e35e1d132131c7759
CTR281 <chmartin2.81@gmail.com>
2024-04-05 09:12:50 +0200

    :sparkles: add MakeGood (daily)

---
+++ Leetcode/problems/1-easy/MakeGood.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._1_easy
+{
+    internal class MakeGood // https://leetcode.com/problems/make-the-string-great/
+    {
+        public class Solution
+        {
+            public string MakeGood(string s)
+            {
+                Stack<char> stack = new Stack<char>();
+    
+                for (int k = 0; k < s.Length; k++)
+                {
+                    if (stack.Count > 0 && char.ToLower(stack.Peek()) == char.ToLower(s[k]) && stack.Peek() != s[k]) stack.Pop();
+                    else stack.Push(s[k]);
+                }
+
+                return new string(stack.Reverse().ToArray());
+            }
+        }
+    }
+}
