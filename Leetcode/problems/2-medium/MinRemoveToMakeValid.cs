e1c532bfcbefdf6c3fee3608473e8ba6e2a730db
CTR281 <chmartin2.81@gmail.com>
2024-04-06 14:53:14 +0200

    :sparkles: add MinRemoveToMakeValid (daily)

---
+++ Leetcode/problems/2-medium/MinRemoveToMakeValid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class MinRemoveToMakeValid // https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+    {
+        public class Solution
+        {
+            public string MinRemoveToMakeValid(string s)
+            {
+                int count = 0;
+                char[] chars = s.ToCharArray();
+                for(int k = 0; k < chars.Length; k++)
+                {
+                    if (chars[k] == ')')
+                    {
+                        if (count > 0) count--;
+                        else chars[k] = ' ';
+                    }
+                    else if (chars[k] == '(') count++;
+                }
+                count = 0;
+                for (int k = chars.Length - 1; k >= 0; k--)
+                {
+                    if (chars[k] == '(')
+                    {
+                        if (count > 0) count--;
+                        else chars[k] = ' ';
+                    }
+                    else if (chars[k] ==  ')') count++;
+                }
+                StringBuilder stringBuilder = new StringBuilder();
+                foreach (char c in chars) if (!char.IsWhiteSpace(c)) stringBuilder.Append(c);
+                return stringBuilder.ToString();
+            }
+        }
+    }
+}
