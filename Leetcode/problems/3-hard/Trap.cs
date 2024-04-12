198dc447118dde69ba05a3c609caec12ed8817b8
CTR281 <chmartin2.81@gmail.com>
2024-04-12 20:31:59 +0200

    :sparkles: add Trap (daily)

---
+++ Leetcode/problems/3-hard/Trap.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._3_hard
+{
+    internal class Trap // https://leetcode.com/problems/trapping-rain-water/
+    {
+        public class Solution
+        {
+            public int Trap(int[] height)
+            {
+                int n = height.Length;
+                int[] right = new int[n];
+                int[] left = new int[n];
+                int max;
+
+                max = 0;
+                for (int k = 0; k < n - 1; k++)
+                {
+                    if (height[max] < height[k]) max = k;
+                    left[k + 1] = max;
+                }
+
+                max = n - 1;
+                left[n - 1] = max;
+                for (int k = n - 1; k > 0; k--)
+                {
+                    if (height[max] < height[k]) max = k;
+                    right[k - 1] = max;
+                }
+
+                int result = 0;
+                for (int k = 0; k < n; k++)
+                {
+                    result += Math.Max(0, Math.Min(height[left[k]], height[right[k]]) - height[k]);
+                }
+
+                return result;
+            }
+        }
+    }
+}
