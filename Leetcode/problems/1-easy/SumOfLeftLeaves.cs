1323da2d9fdd6375de8995f6e3d4d96bdc208bd8
CTR281 <chmartin2.81@gmail.com>
2024-04-14 09:43:06 +0200

    :sparkles: add SumOfLeftLeaves (daily)

---
+++ Leetcode/problems/1-easy/SumOfLeftLeaves.cs
@@ -0,0 +1,26 @@
+﻿using Leetcode.models.leetcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._1_easy
+{
+    internal class SumOfLeftLeaves // https://leetcode.com/problems/sum-of-left-leaves/
+    {
+        public class Solution
+        {
+            private int dfs(TreeNode? node, bool isLeft)
+            {
+                if (node == null) return 0;
+                if (node.left == null && node.right == null && isLeft) return node.val;
+                return dfs(node.left, true) + dfs(node.right, false);
+            }
+            public int SumOfLeftLeaves(TreeNode root)
+            {
+                return dfs(root, false);
+            }
+        }
+    }
+}
