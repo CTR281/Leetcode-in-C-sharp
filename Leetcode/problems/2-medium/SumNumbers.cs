662d64a16f087d8a34c9b5ef78a747a159543ad6
CTR281 <chmartin2.81@gmail.com>
2024-04-15 09:22:13 +0200

    :sparkles: add SumNumbers (daily)

---
+++ Leetcode/problems/2-medium/SumNumbers.cs
@@ -0,0 +1,27 @@
+﻿using Leetcode.models.leetcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class SumNumbers // https://leetcode.com/problems/sum-root-to-leaf-numbers/
+    {
+        public class Solution
+        {
+            private int dfs(TreeNode node, int result)
+            {
+                if (node == null) return 0;
+                result = 10 * result + node.val;
+                return Math.Max(result, dfs(node.left, result) + dfs(node.right, result));
+            }
+
+            public int SumNumbers(TreeNode root)
+            {
+                return dfs(root, 0);
+            }
+        }
+    }
+}
