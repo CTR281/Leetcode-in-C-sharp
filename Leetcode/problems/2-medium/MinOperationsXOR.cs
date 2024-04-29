342c3f2803546887303317188acda0bd03fd1d88
CTR281 <chmartin2.81@gmail.com>
2024-04-29 08:52:55 +0200

    :sparkles: add MinOperationsXOR (daily)

---
+++ Leetcode/problems/2-medium/MinOperationsXOR.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class MinOperationsXOR // https://leetcode.com/problems/minimum-number-of-operations-to-make-array-xor-equal-to-k/
+    {
+        public class Solution
+        {
+            public int MinOperations(int[] nums, int k)
+            {
+                int numsXor = 0;
+                foreach (int num in nums) numsXor ^= num;
+                int result = 0;
+                while (Math.Max(numsXor, k) > 0)
+                {
+                    if ((numsXor & 1) != (k & 1)) result++;
+                    numsXor >>= 1;
+                    k >>= 1;
+                }
+                return result;
+            }
+        }
+    }
+}
