874ab1f42e9eb460a1a17ee1767a5456413cefed
CTR281 <chmartin2.81@gmail.com>
2024-05-01 23:20:53 +0200

    :sparkles: add ReversePrefix (daily)

---
+++ Leetcode/problems/1-easy/ReversePrefix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._1_easy
+{
+    internal class ReversePrefix // https://leetcode.com/problems/reverse-prefix-of-word/
+    {
+        public class Solution
+        {
+            public string ReversePrefix(string word, char ch)
+            {
+                List<char> chars = new List<char>();
+                int k;
+
+                for (k = 0; k < word.Length; k++)
+                {
+                    chars.Add(word[k]);
+                    if (ch == word[k])
+                    {
+                        chars.Reverse();
+                        k++;
+                        break;
+                    }
+                }
+                while (k < word.Length)
+                {
+                    chars.Add(word[k++]);
+                }
+
+                return new string(chars.ToArray());
+            }
+        }
+    }
+}
