12a978cdaf7e4910e1c7d713b9f999144260c34b
CTR281 <chmartin2.81@gmail.com>
2024-05-02 09:46:07 +0200

    :sparkles: add FindMaxK (daily)

---
+++ Leetcode/problems/1-easy/FindMaxK.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._1_easy
+{
+    internal class FindMaxK // https://leetcode.com/problems/largest-positive-integer-that-exists-with-its-negative/
+    {
+        public class Solution
+        {
+            public int FindMaxK(int[] nums)
+            {
+                HashSet<int> negs = new HashSet<int>();
+                foreach (int num in nums)
+                {
+                    if (num < 0) negs.Add(num);
+                }
+                int result = -1;
+                foreach (int num in nums)
+                {
+                    if (num < 0) continue;
+                    if (num > result && negs.Contains(-num))
+                    {
+                        result = num;
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
