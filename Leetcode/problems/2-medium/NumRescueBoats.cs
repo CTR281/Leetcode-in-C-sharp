1c41b529db69c75b0e3dfd11964eab909c098360
CTR281 <chmartin2.81@gmail.com>
2024-05-04 21:09:49 +0200

    :sparkles: add NumRescueBoats (daily)

---
+++ Leetcode/problems/2-medium/NumRescueBoats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class NumRescueBoats // https://leetcode.com/problems/boats-to-save-people/
+    {
+        public class Solution
+        {
+            public int NumRescueBoats(int[] people, int limit)
+            {
+                Array.Sort(people);
+                int result = 0;
+
+                int start = 0, end = people.Length - 1;
+                while (start <= end)
+                {
+                    if (people[end] + people[start] <= limit) start++;
+                    end--;
+                    result++;
+                }
+                return result;
+            }
+        }
+    }
+}
