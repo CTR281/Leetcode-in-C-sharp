dc1e75672a3e18aa1091e493013dcacc7c56cc80
CTR281 <chmartin2.81@gmail.com>
2024-05-05 10:58:08 +0200

    :sparkles: add DeleteNode (daily)

---
+++ Leetcode/problems/2-medium/DeleteNode.cs
@@ -0,0 +1,21 @@
+﻿using Leetcode.models.leetcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class DeleteNode // https://leetcode.com/problems/delete-node-in-a-linked-list/
+    {
+        public class Solution
+        {
+            public void DeleteNode(ListNode node)
+            {
+                node.val = node.next.val;
+                node.next = node.next.next;
+            }
+        }
+    }
+}
