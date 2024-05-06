88a80dcf8a2280d47280d9039e556da78ccad600
CTR281 <chmartin2.81@gmail.com>
2024-05-06 11:12:26 +0200

    :sparkles: add RemoveNodes (daily)

---
+++ Leetcode/problems/2-medium/RemoveNodes.cs
@@ -0,0 +1,32 @@
+﻿using Leetcode.models.leetcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class RemoveNodes // https://leetcode.com/problems/remove-nodes-from-linked-list/
+    {
+        public class Solution
+        {
+            private int dfs(ListNode node, ListNode? parent, int max)
+            {
+                if (node == null) return max;
+                max = Math.Max(node.val, dfs(node.next, node, max));
+                if (node.val < max)
+                {
+                    if (parent != null) parent.next = node.next;
+                    else return -1;
+                }
+                return max;
+            }
+
+            public ListNode RemoveNodes(ListNode head)
+            {
+                return dfs(head, null, int.MinValue) == -1 ? head.next : head;
+            }
+        }
+    }
+}
