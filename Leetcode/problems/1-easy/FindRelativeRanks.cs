c6280dafb6c16266f869e1c62a4d9a37ce5f2dad
CTR281 <chmartin2.81@gmail.com>
2024-05-08 09:49:29 +0200

    :sparkles: add FindRelativeRanks (daily)

---
+++ Leetcode/problems/1-easy/FindRelativeRanks.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._1_easy
+{
+    internal class FindRelativeRanks // https://leetcode.com/problems/relative-ranks/
+    {
+        public class Solution
+        {
+            public string[] FindRelativeRanks(int[] score)
+            {
+                int[] scoreSorted = new int[score.Length];
+                score.CopyTo(scoreSorted, 0);
+                Array.Sort(scoreSorted, (a , b) => b - a);
+                Dictionary<int, int> ranks = new Dictionary<int, int>();
+
+                for (int k = 0; k < score.Length; k++)
+                {
+                    ranks[scoreSorted[k]] = k + 1;
+                }
+
+                string[] result = new string[score.Length];
+                for (int k = 0; k < score.Length; k++)
+                {
+                    int rank = ranks[score[k]];
+                    result[k] = rank switch
+                    {
+                        3 => "Bronze Medal",
+                        2 => "Silver Medal",
+                        1 => "Gold Medal",
+                        int x => x.ToString(),
+                    };
+                }
+                return result;
+            }
+        }
+    }
+}
