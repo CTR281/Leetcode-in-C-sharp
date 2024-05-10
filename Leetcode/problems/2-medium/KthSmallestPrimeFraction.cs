e117700efbf9c43ee11c3970ba4d4249e8f0de06
CTR281 <chmartin2.81@gmail.com>
2024-05-10 10:42:56 +0200

    :sparkles: add KthSmallestPrimeFraction (daily)

---
+++ Leetcode/problems/2-medium/KthSmallestPrimeFraction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class KthSmallestPrimeFraction // https://leetcode.com/problems/k-th-smallest-prime-fraction/
+    {
+        public class Solution
+        {
+            public int[] KthSmallestPrimeFraction(int[] arr, int k)
+            {
+                int n = arr.Length;
+                PriorityQueue<int[], double> pq = new PriorityQueue<int[], double>();
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    pq.Enqueue([i, n - 1], 1.0 * arr[i] / arr[n - 1]);
+                }
+
+                while (--k > 0)
+                {
+                    pq.TryDequeue(out int[] nextFraction, out double value);
+                    if (nextFraction[0] < nextFraction[1])
+                    {
+                        pq.Enqueue([nextFraction[0], nextFraction[1] - 1], 1.0 * arr[nextFraction[0]] / arr[nextFraction[1] - 1]);
+                    }
+                }
+
+                int[] result = pq.Dequeue();
+                return [arr[result[0]], arr[result[1]]];
+            }
+        }
+    }
+}
