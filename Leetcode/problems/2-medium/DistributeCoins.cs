db909342c534a38a48e2aac754373a5b158d3484
CTR281 <chmartin2.81@gmail.com>
2024-05-19 02:14:53 +0200

    :sparkles: add DistributeCoins (daily)

---
+++ Leetcode/problems/2-medium/DistributeCoins.cs
@@ -0,0 +1,31 @@
+﻿using Leetcode.models.leetcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class DistributeCoins // https://leetcode.com/problems/distribute-coins-in-binary-tree/
+    {
+        public class Solution
+        {
+            private int result;
+
+            private int dfs(TreeNode node)
+            {
+                if (node is null) return 0;
+                int leftCoins = dfs(node.left);
+                int rightCoins = dfs(node.right);
+                result += Math.Abs(leftCoins) + Math.Abs(rightCoins);
+                return node.val - 1 + leftCoins + rightCoins;
+            }
+            public int DistributeCoins(TreeNode root)
+            {
+                dfs(root);
+                return result;
+            }
+        }
+    }
+}
