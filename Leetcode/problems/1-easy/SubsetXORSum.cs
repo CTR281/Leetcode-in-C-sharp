2c9183dbd42ff267e8dfeac523497d3fcea0f21f
CTR281 <chmartin2.81@gmail.com>
2024-05-20 09:27:08 +0200

    :sparkles: add SubsetXORSum (daily)

---
+++ Leetcode/problems/1-easy/SubsetXORSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._1_easy
+{
+    internal class SubsetXORSum // https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+    {
+        public class Solution
+        {
+            public int SubsetXORSum(int[] nums)
+            {
+                int result = 0;
+                for (int mask = 0; mask < 2 << nums.Length; mask++)
+                {
+                    int total = 0;
+                    for (int k = 0; k < nums.Length; k++)
+                    {
+                        if ((mask >> k & 1) == 1)
+                        {
+                            total ^= nums[k];
+                        }
+                    }
+                    result += total;
+                }
+                return result;
+            }
+        }
+    }
+}
