0c225ac6c623ee0a8d466e54ef56ec5647dd73bf
CTR281 <chmartin2.81@gmail.com>
2024-05-21 20:04:35 +0200

    :sparkles: add Subsets (daily)

---
+++ Leetcode/problems/2-medium/Subsets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class Subsets // https://leetcode.com/problems/subsets/
+    {
+        public class Solution
+        {
+            public IList<IList<int>> Subsets(int[] nums)
+            {
+                List<IList<int>> result = new List<IList<int>>();
+                for (int mask = 0; mask < 1 << nums.Length; mask++)
+                {
+                    List<int> set = new List<int>();
+                    for (int k = 0; k < nums.Length; k++)
+                    {
+                        if (((mask >> k) & 1) == 1) set.Add(nums[k]);
+                    }
+                    result.Add(set);
+                }
+                return result;
+            }
+        }
+    }
+}
