2f5e59c8d0a96b7ddb898b81e4a55b284faebed6
CTR281 <chmartin2.81@gmail.com>
2024-05-24 18:56:14 +0200

    :sparkles: add MaxScoreWords (daily)

---
+++ Leetcode/problems/3-hard/MaxScoreWords.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._3_hard
+{
+    internal class MaxScoreWords // https://leetcode.com/problems/maximum-score-words-formed-by-letters/
+    {
+        public class Solution
+        {
+            private int backtrack(int start, int[] availableLetters, int result, string[] words, int[] score)
+            {
+                if (start == words.Length) return result;
+                int wordScore = 0;
+                for (int k = 0; k < words[start].Length; k++)
+                {
+                    char letter = words[start][k];
+                    if (availableLetters[letter - 'a'] == 0) return result;
+                    wordScore += score[letter - 'a'];
+                    availableLetters[letter - 'a']--;
+                }
+                result += wordScore;
+                int currentScore = result;
+                for (int k = start; k < words.Length; k++)
+                {
+                    result = Math.Max(result, backtrack(k + 1, (int[])availableLetters.Clone(), currentScore, words, score));
+                }
+                return result;
+            }
+
+            public int MaxScoreWords(string[] words, char[] letters, int[] score)
+            {
+                int[] lettersFreq = new int[26];
+                for (int k = 0; k < letters.Length; k++)
+                {
+                    lettersFreq[letters[k] - 'a']++;
+                }
+                int result = 0;
+                for (int k = 0; k < words.Length; k++)
+                {
+                    result = Math.Max(result, backtrack(k, (int[]) lettersFreq.Clone(), 0, words, score));
+                }
+                return result;
+            }
+        }
+    }
+}
