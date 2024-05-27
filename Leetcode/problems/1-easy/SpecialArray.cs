ad79765f712a641c8efab146499b1203e9a514d4
CTR281 <chmartin2.81@gmail.com>
2024-05-27 23:49:01 +0200

    :sparkles: add SpecialArray (daily)

---
+++ Leetcode/problems/1-easy/SpecialArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._1_easy
+{
+    internal class SpecialArray // https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/
+    {
+        public class Solution
+        {
+            public int SpecialArray(int[] nums)
+            {
+                for (int i = 1; i <= nums.Length; i++)
+                {
+                    int count = 0;
+                    for (int j = 0; j < nums.Length; j++)
+                    {
+                        if (nums[j] >= i) count++;
+                        if (count > i) break;
+                    }
+                    if (count == i) return i;
+                }
+                return -1;
+            }
+        }
+    }
+}
