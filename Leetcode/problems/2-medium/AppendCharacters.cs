eba0e59949b4531343094675fe8dc6c02fa425c7
CTR281 <chmartin2.81@gmail.com>
2024-06-03 09:07:29 +0200

    :sparkles: add AppendCharacters (daily)

---
+++ Leetcode/problems/2-medium/AppendCharacters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class AppendCharacters // https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/
+    {
+        public class Solution
+        {
+            public int AppendCharacters(string s, string t)
+            {
+                int sCursor = 0, tCursor = 0, result = t.Length;
+                while (tCursor < t.Length && sCursor < s.Length)
+                {
+                    if (s[sCursor] == t[tCursor])
+                    {
+                        result--;
+                        tCursor++;
+                    }
+                    sCursor++;
+                }
+                return result;
+            }
+        }
+    }
+}
