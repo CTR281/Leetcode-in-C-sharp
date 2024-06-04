571cf565cc9515006c8daa3b3d97a81c943285d2
CTR281 <chmartin2.81@gmail.com>
2024-06-04 17:23:37 +0200

    :sparkles: add LongestPalindrome (daily)

---
+++ Leetcode/problems/1-easy/LongestPalindrome.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._1_easy
+{
+    internal class LongestPalindrome // https://leetcode.com/problems/longest-palindrome/
+    {
+        public class Solution
+        {
+            public int LongestPalindrome(string s)
+            {
+                Dictionary<char, int> lettersCount = new Dictionary<char, int>();
+                foreach (char c in s)
+                {
+                    if (!lettersCount.ContainsKey(c)) lettersCount.Add(c, 0);
+                    lettersCount[c]++;
+                }
+                int result = 0;
+                bool tookOdd = false;
+                foreach ((char letter, int count) in lettersCount)
+                {
+                    if (count % 2 == 0) result += count;
+                    else if (!tookOdd)
+                    {
+                        tookOdd = true;
+                        result += count;
+                    }
+                    else
+                    {
+                        result += count - 1;
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
