c03513c373d75efe59d76bb01e3e94dd4fc38870
CTR281 <chmartin2.81@gmail.com>
2024-06-05 20:26:04 +0200

    :sparkles: add CommonChars (daily)

---
+++ Leetcode/problems/1-easy/CommonChars.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._1_easy
+{
+    internal class CommonChars // https://leetcode.com/problems/find-common-characters/
+    {
+        public class Solution
+        {
+            public IList<string> CommonChars(string[] words)
+            {
+                int[] charCount = new int[26];
+                Array.Fill(charCount, int.MaxValue);
+                foreach (string word in words)
+                {
+                    int[] wordChars = new int[26];
+                    foreach (char c in word)
+                    {
+                        wordChars[c - 'a']++;
+                    }
+                    for (int k = 0; k < 26; k++)
+                    {
+                        charCount[k] = Math.Min(charCount[k], wordChars[k]);
+                    }
+                }
+                List<string> result = new List<string>();
+                for (int k = 0; k < 26; k++)
+                {
+                    while (charCount[k]-- > 0) result.Add(((char)('a' + k)).ToString());
+                }
+                return result;
+            }
+        }
+    }
+}
