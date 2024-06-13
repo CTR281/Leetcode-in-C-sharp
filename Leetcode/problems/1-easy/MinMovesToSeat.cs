e7f71df4b8218848423c5141868c37844c66ecb8
CTR281 <chmartin2.81@gmail.com>
2024-06-13 09:25:29 +0200

    :sparkles: Add MinMovesToSeat (daily)

---
+++ Leetcode/problems/1-easy/MinMovesToSeat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._1_easy
+{
+    internal class MinMovesToSeat // https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/
+    {
+        public class Solution
+        {
+            public int MinMovesToSeat(int[] seats, int[] students)
+            {
+                Array.Sort(seats);
+                Array.Sort(students);
+
+                int result = 0;
+                for (int k = 0; k < seats.Length; k++) result += Math.Abs(students[k] - seats[k]);
+                return result;
+            }
+        }
+    }
+}
