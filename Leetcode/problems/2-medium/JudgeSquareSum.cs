5545d567dc839022e22d514f2555c52ce107471b
CTR281 <chmartin2.81@gmail.com>
2024-06-17 19:13:25 +0200

    :sparkles: add JudgeSquareSum (daily)

---
+++ Leetcode/problems/2-medium/JudgeSquareSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class JudgeSquareSum // https://leetcode.com/problems/sum-of-square-numbers/
+    {
+        public class Solution
+        {
+            public bool JudgeSquareSum(int c)
+            {
+                long k = 0;
+
+                while (k * k <= c)
+                {
+                    double sqrt = Math.Sqrt(c - k * k);
+                    if (sqrt == (int)sqrt) return true;
+                    k++;
+                }
+                return false;
+            }
+        }
+    }
+}
