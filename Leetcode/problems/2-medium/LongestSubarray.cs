34b3f05e37905218ba4e23b46c70a379e72cbd15
CTR281 <chmartin2.81@gmail.com>
2025-07-30 11:01:54 +0200

    :sparkles: add LongestSubarray (daily)

---
+++ Leetcode/problems/2-medium/LongestSubarray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.problems._2_medium
+{
+    internal class LongestSubarray // https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/
+    {
+        public class Solution // Time complexity: O(N)
+        {
+            public int LongestSubarray(int[] nums)
+            {
+                int max = 0;
+                int result = 0;
+                int current = 0;
+                for (int k = 0; k < nums.Length; k++)
+                {
+                    if (nums[k] < max)
+                    {
+                        result = Math.Max(result, current);
+                        current = 0;
+                    } else if (nums[k] == max)
+                    {
+                        current++;
+                    }
+                    else
+                    {
+                        max = nums[k];
+                        result = 1;
+                        current = 1;
+                    }
+                }
+                return Math.Max(result, current);
+            }
+        }
+    }
+}
