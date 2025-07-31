5e7fe7ea77d619f8bb27978be2faec0837173055
CTR281 <chmartin2.81@gmail.com>
2025-07-31 23:29:44 +0200

    :sparkles: add SubarrayBitwiseORs (daily)

---
+++ Leetcode/problems/2-medium/SubarrayBitwiseORs.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode.problems._2_medium;
+
+internal class SubarrayBitwiseORs
+{
+    public class Solution
+    {
+        public int SubarrayBitwiseORs(int[] arr)
+        {
+            HashSet<int> result = new HashSet<int>();
+            HashSet<int> cur = new HashSet<int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                HashSet<int> cur2 = new HashSet<int>();
+                foreach (int item in cur) cur2.Add(item | arr[i]);
+                cur2.Add(arr[i]);
+                cur = cur2;
+                result.UnionWith(cur2);
+            }
+
+            return result.Count;
+        }
+    }
+}
