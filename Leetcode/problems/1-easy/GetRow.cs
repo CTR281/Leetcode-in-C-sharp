9fb3e08ab7e52f630f20cba8f34e408d3a1b0c01
CTR281 <chmartin2.81@gmail.com>
2025-08-01 11:57:20 +0200

    :sparkles: add GetRow

---
+++ Leetcode/problems/1-easy/GetRow.cs
@@ -0,0 +1,20 @@
+﻿namespace Leetcode.problems._1_easy;
+
+public class GetRow
+{
+    public class Solution { // https://leetcode.com/problems/pascals-triangle-ii/
+        public IList<int> GetRow(int rowIndex) { // Time complexity: O(rowIndex^2); Space complexity O(rowIndex)
+            var result = new int[rowIndex + 1];
+            Array.Fill(result, 0);
+            result[0] = 1;
+            for (int i = 0; i <= rowIndex; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    result[j] += result[j - 1];
+                }
+            }
+            return result;
+        }
+    }
+}
