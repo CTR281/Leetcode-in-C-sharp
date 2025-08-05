6dabd02542686bbee2b247a22b185d2337c68e74
CTR281 <chmartin2.81@gmail.com>
2025-08-06 00:25:00 +0200

    :sparkles: add NumOfUnplacedFruits (daily)

---
+++ Leetcode/problems/1-easy/NumOfUnplacedFruits.cs
@@ -0,0 +1,22 @@
+﻿namespace Leetcode.problems._1_easy;
+
+public class NumOfUnplacedFruits
+{
+    public class Solution { // Time complexity: O(N^2) (N(N+1)/2 worst case)
+        public int NumOfUnplacedFruits(int[] fruits, int[] baskets) {
+            var result = 0;
+            foreach (var fruit in fruits)
+            {
+                var j = 0;
+                while (j < baskets.Length && fruit > baskets[j])
+                {
+                    j++;
+                }
+
+                if (j == baskets.Length) result++;
+                else baskets[j] = 0;
+            }
+            return result;
+        }
+    }
+}
